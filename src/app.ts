fb94022af18420b2cd83708c84bbe2c48e6dd53a
AfnanFerdousi <afnanferdousi550@gmail.com>
2023-09-20 00:08:34 +0600

    [DONE] Setting up project and error handing

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import globalErrorHandler from "./app/middlewares/globarErrorHandler";
+import routes from "./app/routes/routes";
+import httpStatus from "http-status";
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("env");
+// application routes
+app.use("/api/v1/", routes);
+
+// global handler
+app.use(globalErrorHandler);
+
+// handle not found route
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(httpStatus.NOT_FOUND).json({
+        success: false,
+        message: "Not Found",
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: "API Not Found",
+            },
+        ],
+    });
+    next();
+});
+
+export default app;
